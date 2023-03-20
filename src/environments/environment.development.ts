d72db9e761deb7dd692f0f33f08d602073b9ad49
bahir24 <m.bahirev@wemakefab.ru>
2023-03-20 12:52:28 +0300

    update debug environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 import {IEnvironment} from "../app/models/environment";
 const PROTOCOL = 'http';
-const HOST = 'localhost';
+const HOST = '178.208.86.93';
 const PORT = 3000;
 
 export const environment: IEnvironment = {
